c31268fe2efc2ff6a84a914a40b08d7aa4da8a47
Mahesh-Pradhan <Dk482389@gmail.com>
2023-11-09 17:04:22 +0530

    Multi-dimensional array

--- Day6.java
+++ Day6.java
@@ -3,14 +3,14 @@
 class Day6{
     public static void main(String[] args){
 
-        int nums[][] = new int[3][4];
+        int nums[][] = new int[3][4]; // Creating an multidimensional array of type int
 
         for(int i=0;i<3;i++){
 
             for (int j=0;j<4;j++){
 
-                nums[i][j]=(int)(Math.random()*10);
-            }
+                nums[i][j]=(int)(Math.random()*10); // taking random values and multiplying it by 10 as random()function
+            }                                       // return a double value, and then typecasting it to int type.
             
         }
         for(int i=0;i<3;i++){
