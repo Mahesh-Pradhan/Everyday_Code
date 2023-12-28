d3931304e73f8d5a960166671b7ebef5a84ff2c6
Mahesh-Pradhan <Dk482389@gmail.com>
2023-12-28 20:54:00 +0530

    Prime number or not

--- prime.java
+++ prime.java
@@ -5,12 +5,12 @@ public class prime{
         int m,i,flag=0;
         m=n/2;
         if (n==0 || n==1){
-            System.out.println(n + "is not a prime a number");
+            System.out.println(n + " is not a prime a number");
         }
         else{
             for(i=2;i<=m;i++){
                 if (n%i==0){
-                    System.out.println(n+"is not a prime number");
+                    System.out.println(n+" is not a prime number");
                     flag=1;
                     break;
                 }
